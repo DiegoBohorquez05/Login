4a839decaa51026e767e09a6103e4cb8215a382a
DiegoBohorquez05 <113608171+DiegoBohorquez05@users.noreply.github.com>
2023-01-23 17:59:42 -0500

    js terminado

--- script.js
+++ script.js
@@ -6,13 +6,20 @@ const inputPassword=document.getElementById('txt-password').value
 const entrar=document.getElementById('entrar')
 
 entrar.addEventListener('click',()=>{
-    if(inputUsuario===user_name && inputPassword===password){
-        const token='asiodufgiasdvbashdvashdcblaksd';
-        console.log(token);
-        localStorage.setItem("token", token);
-        window.location.href="Pagina_inicio.html"
+    if(inputUsuario==='' || inputPassword===''){
+        alert('Hay espacios vacios en la peticion')
+        //AQUI INTENTE VOLVER A PONER ESPACIOS EN BLANCO PERO NO SALE
+//        inputUsuario.innerHTML=''
+//        inputPassword.innerHTML=''
     }else{
-        alert('Algo esta mal')
+        if(inputUsuario===user_name && inputPassword===password){
+            const token='asiodufgiasdvbashdvashdcblaksd';
+            console.log(token);
+            localStorage.setItem("token", token);
+            window.location.href="Pagina_inicio.html"
+        }else{
+            alert('Algo esta mal')
+        }
     }
 })
 
