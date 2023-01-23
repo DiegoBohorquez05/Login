c43446a095a57b7652010162597d396f2e79fe83
DiegoBohorquez05 <judiboor@gmail.com>
2023-01-23 09:58:14 -0500

    token

--- script.js
+++ script.js
@@ -6,11 +6,6 @@ const inputPassword=document.getElementById('txt-password').value
 const entrar=document.getElementById('entrar')
 
 entrar.addEventListener('click',()=>{
-    // console.log(typeof(password))
-    // console.log(inputUsuario===user_name?true:false)
-    // console.log(inpuasiodufgiasdvbashdvashdcblaksdtPassword===password?true:false)
-    // console.log(inputUsuario)
-    // console.log(inputPassword)
     if(inputUsuario===user_name && inputPassword===password){
         const token='asiodufgiasdvbashdvashdcblaksd';
         console.log(token);
