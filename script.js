3a002fc85d65db6aaffe409a1f306b5ecc526e48
DiegoBohorquez05 <judiboor@gmail.com>
2023-01-23 08:44:15 -0500

    login

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-guardad_localstorage()
+// guardad_localstorage()
 
-function guardad_localstorage(){
-    let usuario = {
-        user_name: "admin",
-        password: "123456789"
-    }
+// function guardad_localstorage(){
+//     let usuario = {
+
+//     }
     
-    localStorage.setItem("user", usuario.user_name)
-    localStorage.setItem("password", usuario.password)
-};
+//     localStorage.setItem("user", usuario.user_name)
+//     localStorage.setItem("password", usuario.password)
+// };
+
+const user_name = "admin"
+const password = "123456789"
+
+const inputUsuario = document.getElementById('text-user')
+const inputContraseña=document.getElementById('txt-password')
+
+if(user_name===inputUsuario && password===inputContraseña){
+    console.log('asiodufgiasdvbashdvashdcblaksd')
+}else{
+    alert('No exsite')
+}
